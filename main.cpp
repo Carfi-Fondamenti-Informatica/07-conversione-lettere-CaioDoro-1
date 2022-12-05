808a4edae63e43c23f7362a2f7df6a7df4a50b5f
CaioDoro <117940103+CaioDoro@users.noreply.github.com>
2022-12-05 18:02:06 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+using namespace std;
 
-int main(){
-  return 0;
-}
+int main() {
+    char c;
+    cin >> c;
+    int n = int(c);
+    int a;
+    char r;
+
+    if (n > 64 && n < 91) {
+        a = n + 32;
+        r = char(a);
+        cout << r << endl;
+    } else if (n > 96 && n < 123) {
+        a = n - 32;
+        r = char(a);
+        cout << r << endl;
+
+    } else
+        cout << "errore" << endl;
+return 0;}
